0b17bbdcca680315eab03f174c3e7cdff6bff6f5
Michael Novotny <manovotny@manovotny-rmbp.attlocal.net>
2014-06-07 16:34:35 -0500

    Adds save capabilities for tips.

--- inc/meta-boxes/tips.php
+++ inc/meta-boxes/tips.php
@@ -4,16 +4,18 @@
  * @author Michael Novotny <manovotny@gmail.com>
  */
 
-// Register meta box.
 add_action( 'add_meta_boxes_recipe', 'wp_recipe_add_tips_meta_box' );
-//add_action( 'save_post', array( $this, 'save_pinterest_meta_box' ) );
+add_action( 'save_post', 'wp_recipe_save_tips_meta_box' );
 
+/**
+ * Adds tips meta box.
+ */
 function wp_recipe_add_tips_meta_box() {
 
     add_meta_box(
         'wp-recipe-tips',
         'Tips',
-        'wp_recipe_tips_meta_box',
+        'wp_recipe_display_tips_meta_box',
         'recipe',
         'normal',
         'high'
@@ -21,13 +23,45 @@ function wp_recipe_add_tips_meta_box() {
 
 }
 
-function wp_recipe_tips_meta_box() {
+/**
+ * Displays tips meta box.
+ */
+function wp_recipe_display_tips_meta_box() {
+
+    global $post;
+
+    wp_nonce_field( 'wp-recipe-tips', 'wp-recipe-tips-nonce' );
+
+    $tips = get_post_meta( $post->ID, 'wp-recipe-tips', true );
 
     $settings = array(
         'drag_drop_upload'  => true,
         'textarea_rows'     => 3
     );
 
-    wp_editor( '', 'wp_recipe_tips', $settings );
+    wp_editor( $tips, 'wp_recipe_tips', $settings );
+
+}
+
+/**
+ * Saves tips.
+ *
+ * @param string $post_id Post id.
+ */
+function wp_recipe_save_tips_meta_box( $post_id ) {
+
+    if ( empty( $_POST ) || 'recipe' !== $_POST[ 'post_type' ] ) {
+
+        return;
+
+    }
+
+    $wp_recipe = WP_Recipe::get_instance();
+
+    if ( $wp_recipe->can_user_save( $post_id, 'wp-recipe-tips', 'wp-recipe-tips-nonce' ) ) {
+
+        update_post_meta( $post_id, 'wp-recipe-tips', $_POST[ 'wp_recipe_tips' ] );
+
+    }
 
 }
