48e466fdef584725fa0aa5506a736336aa23bd9a
Michael Novotny <manovotny@manovotny-rmbp.attlocal.net>
2014-06-07 15:33:32 -0500

    Adds save capabilities to yield.

--- inc/meta-boxes/yield.php
+++ inc/meta-boxes/yield.php
@@ -7,12 +7,15 @@
 add_action( 'add_meta_boxes_recipe', 'wp_recipe_add_yield_meta_box' );
 add_action( 'save_post', 'wp_recipe_save_yield_meta_box' );
 
-function wp_recipe_save_yield_meta_box() {
+/**
+ * Adds yield meta box.
+ */
+function wp_recipe_add_yield_meta_box() {
 
     add_meta_box(
         'wp-recipe-yield',
         'Yield',
-        'wp_recipe_yield_meta_box',
+        'wp_recipe_display_yield_meta_box',
         'recipe',
         'normal',
         'high'
@@ -20,19 +23,51 @@ function wp_recipe_save_yield_meta_box() {
 
 }
 
-function wp_recipe_yield_meta_box() {
+/**
+ * Displays ingredients meta box.
+ */
+function wp_recipe_display_yield_meta_box() {
+
+    global $post;
+
+    wp_nonce_field( 'wp-recipe-yield', 'wp-recipe-yield-nonce' );
+
+    $yield = get_post_meta( $post->ID, 'wp-recipe-yield', true );
 
     $html = '';
 
     $html .= '<fieldset class="wp-recipe-yield">';
         $html .= '<ul class="list">';
             $html .= '<li class="list-item">';
-                $html .= '<label for="yield" class="item-label">Yield</label>';
-                $html .= '<input id="yield" class="item-control" name="yield" type="text" value="" />';
+                $html .= '<label class="item-label" for="wp-recipe-yield">Yield</label>';
+                $html .= '<input class="item-control" id="wp-recipe-yield" name="wp-recipe-yield" type="text" value="' . $yield . '" />';
             $html .= '</li>';
         $html .= '</ul>';
     $html .= '</fieldset>';
 
     echo $html;
+
+}
+
+/**
+ * Saves yield.
+ *
+ * @param string $post_id Post id.
+ */
+function wp_recipe_save_yield_meta_box( $post_id ) {
+
+    if ( 'recipe' !== $_POST[ 'post_type' ] ) {
+
+        return;
+
+    }
+
+    $wp_recipe = WP_Recipe::get_instance();
+
+    if ( $wp_recipe->can_user_save( $post_id, 'wp-recipe-yield', 'wp-recipe-yield-nonce' ) ) {
+
+        update_post_meta( $post_id, 'wp-recipe-yield', $_POST[ 'wp-recipe-yield' ] );
+
+    }
 
 }
