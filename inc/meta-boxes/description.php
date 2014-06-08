5fe7405847103013ac03047d40c743800311fa60
Michael Novotny <manovotny@manovotny-rmbp.attlocal.net>
2014-06-08 11:31:02 -0500

    Adds save capabilities for description.

--- inc/meta-boxes/description.php
+++ inc/meta-boxes/description.php
@@ -4,16 +4,18 @@
  * @author Michael Novotny <manovotny@gmail.com>
  */
 
-// Register meta box.
 add_action( 'add_meta_boxes_recipe', 'wp_recipe_add_description_meta_box' );
-//add_action( 'save_post', array( $this, 'save_pinterest_meta_box' ) );
+add_action( 'save_post', 'wp_recipe_save_description_meta_box' );
 
+/**
+ * Adds description meta box.
+ */
 function wp_recipe_add_description_meta_box() {
 
     add_meta_box(
         'wp-recipe-description',
         'Description',
-        'wp_recipe_description_meta_box',
+        'wp_recipe_display_description_meta_box',
         'recipe',
         'normal',
         'high'
@@ -21,13 +23,45 @@ function wp_recipe_add_description_meta_box() {
 
 }
 
-function wp_recipe_description_meta_box() {
+/**
+ * Displays description meta box.
+ */
+function wp_recipe_display_description_meta_box() {
+
+    global $post;
+
+    wp_nonce_field( 'wp-recipe-description', 'wp-recipe-description-nonce' );
+
+    $description = get_post_meta( $post->ID, 'wp-recipe-description', true );
 
     $settings = array(
         'drag_drop_upload'  => true,
         'textarea_rows'     => 3
     );
 
-    wp_editor( '', 'wp_recipe_description', $settings );
+    wp_editor( $description, 'wp_recipe_description', $settings );
+
+}
+
+/**
+ * Saves description.
+ *
+ * @param string $post_id Post id.
+ */
+function wp_recipe_save_description_meta_box( $post_id ) {
+
+    if ( empty( $_POST ) || 'recipe' !== $_POST[ 'post_type' ] ) {
+
+        return;
+
+    }
+
+    $wp_recipe = WP_Recipe::get_instance();
+
+    if ( $wp_recipe->can_user_save( $post_id, 'wp-recipe-description', 'wp-recipe-description-nonce' ) ) {
+
+        update_post_meta( $post_id, 'wp-recipe-description', $_POST[ 'wp_recipe_description' ] );
+
+    }
 
 }
