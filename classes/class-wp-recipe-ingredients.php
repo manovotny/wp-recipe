c0b9674939cae2eafdbf2225d839049c2475bb62
Michael Novotny <manovotny@manovotny-rmbp.local>
2014-06-09 12:50:59 -0500

    Refactors ingredient strings into a singleton class.

---
+++ classes/class-wp-recipe-ingredients.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * @package WP_Recipe
+ * @author Michael Novotny <manovotny@gmail.com>
+ */
+
+class WP_Recipe_Ingredients {
+
+    /* Properties
+    ---------------------------------------------------------------------------------- */
+
+    /* Id
+    ---------------------------------------------- */
+
+    /**
+     * Recipe ingredients id.
+     *
+     * @var string
+     */
+    protected $id = 'wp-recipe-ingredient';
+
+    /**
+     * Getter method for id.
+     *
+     * @return string Recipe ingredients id.
+     */
+    public function get_id() {
+
+        return $this->slug;
+
+    }
+
+    /* Instance
+    ---------------------------------------------- */
+
+    /**
+     * Instance of the class.
+     *
+     * @var WP_Recipe_Ingredients
+     */
+    protected static $instance = null;
+
+    /**
+     * Get accessor method for instance property.
+     *
+     * @return WP_Recipe_Ingredients Instance of the class.
+     */
+    public static function get_instance() {
+
+        if ( null == self::$instance ) {
+
+            self::$instance = new self;
+
+        }
+
+        return self::$instance;
+
+    }
+
+    /* Nonce
+    ---------------------------------------------- */
+
+    /**
+     * Getter method for nonce.
+     *
+     * @return string Recipe ingredients nonce.
+     */
+    public function get_nonce() {
+
+        return $this->slug . '-nonce';
+
+    }
+
+    /* Slug
+    ---------------------------------------------- */
+
+    /**
+     * Recipe ingredients slug.
+     *
+     * @var string
+     */
+    protected $slug = 'wp-recipe-ingredients';
+
+    /**
+     * Getter method for slug.
+     *
+     * @return string Recipe ingredients slug.
+     */
+    public function get_slug() {
+
+        return $this->slug;
+
+    }
+
+}
--- inc/meta-boxes/ingredients.php
+++ inc/meta-boxes/ingredients.php
@@ -12,11 +12,14 @@ add_action( 'save_post', 'wp_recipe_save_ingredients_meta_box' );
  */
 function wp_recipe_add_ingredients_meta_box() {
 
+    $wp_recipe = WP_Recipe::get_instance();
+    $wp_recipe_ingredients = WP_Recipe_Ingredients::get_instance();
+
     add_meta_box(
-        'wp-recipe-ingredients',
+        $wp_recipe_ingredients->get_slug(),
         'Ingredients',
         'wp_recipe_display_ingredients_meta_box',
-        'recipe',
+        $wp_recipe->get_post_type(),
         'normal',
         'high'
     );
@@ -30,13 +33,15 @@ function wp_recipe_display_ingredients_meta_box() {
 
     global $post;
 
-    wp_nonce_field( 'wp-recipe-ingredients', 'wp-recipe-ingredients-nonce' );
+    $wp_recipe_ingredients = WP_Recipe_Ingredients::get_instance();
 
-    $ingredients = maybe_unserialize( get_post_meta( $post->ID, 'wp-recipe-ingredients', true ) );
+    wp_nonce_field( $wp_recipe_ingredients->get_slug(), $wp_recipe_ingredients->get_nonce() );
+
+    $ingredients = maybe_unserialize( get_post_meta( $post->ID, $wp_recipe_ingredients->get_slug(), true ) );
 
     $html = '';
 
-    $html .= '<fieldset class="wp-recipe-ingredients">';
+    $html .= '<fieldset class="' . $wp_recipe_ingredients->get_slug() . '">';
         $html .= '<button class="add-ingredient button">Add</button>';
         $html .= '<ul class="list ingredients">';
 
@@ -46,7 +51,7 @@ function wp_recipe_display_ingredients_meta_box() {
 
                     $html .= '<li class="ingredient">';
                         $html .= '<label class="item-label">Ingredient</label>';
-                        $html .= '<input class="item-control" name="wp-recipe-ingredient[]" type="text" value="' . $ingredient . '" />';
+                        $html .= '<input class="item-control" name="' . $wp_recipe_ingredients->get_id() . '[]" type="text" value="' . $ingredient . '" />';
                         $html .= '<span class="item-action">';
                             $html .= '<button class="remove-ingredient button">Remove</button>';
                         $html .= '</span>';
@@ -70,17 +75,19 @@ function wp_recipe_display_ingredients_meta_box() {
  */
 function wp_recipe_save_ingredients_meta_box( $post_id ) {
 
-    if ( empty( $_POST ) || 'recipe' !== $_POST[ 'post_type' ] ) {
+    $wp_recipe = WP_Recipe::get_instance();
+
+    if ( empty( $_POST ) || $wp_recipe->get_post_type() !== $_POST[ 'post_type' ] ) {
 
         return;
 
     }
 
-    $wp_recipe = WP_Recipe::get_instance();
+    $wp_recipe_ingredients = WP_Recipe_Ingredients::get_instance();
 
-    if ( $wp_recipe->can_user_save( $post_id, 'wp-recipe-ingredients', 'wp-recipe-ingredients-nonce' ) ) {
+    if ( $wp_recipe->can_user_save( $post_id, $wp_recipe_ingredients->get_slug(), $wp_recipe_ingredients->get_nonce() ) ) {
 
-        update_post_meta( $post_id, 'wp-recipe-ingredients', $_POST[ 'wp-recipe-ingredient' ] );
+        update_post_meta( $post_id, $wp_recipe_ingredients->get_slug(), $_POST[ $wp_recipe_ingredients->get_id() ] );
 
     }
 
