97292005bb616e5275761ef62a8b1ecf51c86e10
Michael Novotny <manovotny@manovotny-rmbp.local>
2014-06-26 20:57:23 -0500

    Adds basic formatting for rendered shortcode.

--- inc/shortcode.php
+++ inc/shortcode.php
@@ -4,7 +4,7 @@
  * @author Michael Novotny <manovotny@gmail.com>
  */
 
-add_shortcode( 'recipe', 'wp_recipe_shortcode' );
+add_shortcode( 'wp-recipe', 'wp_recipe_shortcode' );
 
 /**
  * Renders recipe shortcode.
@@ -12,31 +12,74 @@ add_shortcode( 'recipe', 'wp_recipe_shortcode' );
  * @param array $attributes Shortcode attributes.
  * @return string Rendered shortcode.
  */
-function recipe_shortcode( $attributes ) {
+function wp_recipe_shortcode( $attributes ) {
 
-    // Extract shortcode attributes.
-    extract( shortcode_atts( array( 'id' => '' ), $attributes ) );
+    extract(
+        shortcode_atts(
+            array(
+                'id' => ''
+            ),
+            $attributes
+        )
+    );
 
-    // Check for id.
     if ( empty( $id ) ) {
 
-        // No id.
         return '';
 
     }
 
-    // Get recipe.
-    $recipe = get_post( $id );
+    $recipe_post_meta = get_post_meta( $id );
 
-    // Check for recipe.
-    if ( empty( $recipe ) ) {
+    if ( empty( $recipe_post_meta ) ) {
 
-        // No recipe.
         return '';
 
     }
 
-    // Return recipe content.
-    return $recipe->post_content;
+    $wp_recipe_description = WP_Recipe_Description::get_instance();
+    $wp_recipe_directions = WP_Recipe_Directions::get_instance();
+    $wp_recipe_ingredients = WP_Recipe_Ingredients::get_instance();
+    $wp_recipe_tips = WP_Recipe_Tips::get_instance();
+    $wp_recipe_yield = WP_Recipe_Yield::get_instance();
+
+    $title = get_the_title( $id );
+    $description = $recipe_post_meta[ $wp_recipe_description->get_slug() ][ 0 ];
+    $directions = $recipe_post_meta[ $wp_recipe_directions->get_slug() ][ 0 ];
+    $ingredients = maybe_unserialize( $recipe_post_meta[ $wp_recipe_ingredients->get_slug() ][ 0 ] );
+    $tips = $recipe_post_meta[ $wp_recipe_tips->get_slug() ][ 0 ];
+    $yield = $recipe_post_meta[ $wp_recipe_yield->get_slug() ][ 0 ];
+
+
+    $html = '';
+    $html .= '<section class="recipe">';
+        $html .= '<h1 class="title">' . $title . '</h1>';
+        $html .= '<h2 class="description">' . $description . '</h2>';
+        $html .= '<section class="meta">';
+            $html .= $yield;
+        $html .= '</section>';
+        $html .= '<section class="ingredients">';
+            $html .= '<h3>Ingredients</h3>';
+            $html .= '<ul>';
+
+                foreach ( $ingredients as $ingredient ) {
+
+                    $html .= '<li>' . $ingredient . '</li>';
+
+                }
+
+            $html .= '</ul>';
+        $html .= '</section>';
+        $html .= '<section class="directions">';
+            $html .= '<h3>Directions</h3>';
+            $html .= '<p>' . $directions . '</p>';
+        $html .= '</section>';
+        $html .= '<section class="tips">';
+            $html .= '<h3>Tips</h3>';
+            $html .= '<p>' . $tips . '</p>';
+        $html .= '</section>';
+    $html .= '</section>';
+
+    return $html;
 
 }
