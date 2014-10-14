88f4eb5636f1c27aebbb962d728aee6001d6ed2c
Michael Novotny <manovotny@gmail.com>
2014-10-14 15:56:09 -0500

    Excludes recipes from search results.

--- src/admin/inc/post-type.php
+++ src/admin/inc/post-type.php
@@ -30,7 +30,7 @@ function wp_recipe_post_type() {
     // Create post type arguments.
     $args = array(
         'description'           =>  _x( 'A place to collect all your delicious recipes', 'custom post type description', $domain ),
-        'exclude_from_search'   =>  false,
+        'exclude_from_search'   =>  true,
         'hierarchical'          =>  false,
         'labels'                =>  $labels,
         'menu_position'         =>  5,
