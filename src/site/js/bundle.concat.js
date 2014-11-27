66f4d84f80c749b8c3fab98ce539e21b43272785
Michael Novotny <manovotny@gmail.com>
2014-11-27 15:23:30 -0600

    Replaces simple print styles with more advanced JavaScript solution.
    This is to avoid printing too many pages since `visibility` still takes up layout space.

--- src/site/js/bundle.concat.js
+++ src/site/js/bundle.concat.js
@@ -3,12 +3,26 @@
 
     'use strict';
 
-    function printRecipe() {
+    function printRecipe(event) {
+        var $body = $('body'),
+            $recipe = $(event.currentTarget).parents('.recipe').clone(),
+            printableClass = 'printable-recipe',
+            printableSelector = '.' + printableClass,
+            printButtonSelector = '.print';
+
+        $recipe.addClass(printableClass);
+
+        $recipe.find(printButtonSelector).remove();
+
+        $body.append($recipe);
+
         window.print();
+
+        $body.find(printableSelector).remove();
     }
 
     function init() {
-        $('.print').on('click', printRecipe);
+        $('.recipe .print').on('click', printRecipe);
     }
 
     init();
