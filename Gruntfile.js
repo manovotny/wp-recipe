b406ef77525a7a2eec821bbac6d185670d46f926
Michael Novotny <manovotny@gmail.com>
2014-12-12 14:28:46 -0600

    Updates Grunt tasks.

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,8 +7,7 @@ module.exports = function (grunt) {
     require('load-grunt-config')(grunt, {
         loadGruntTasks: {
             pattern: [
-                'grunt-*',
-                'unpathify'
+                'grunt-*'
             ]
         }
     });
