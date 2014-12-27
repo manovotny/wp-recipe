d8a1a09767a6cf26f7c9d53340b64d7134cc1831
Michael Novotny <manovotny@gmail.com>
2014-12-26 23:46:11 -0600

    Fixes intermitent print issues and adds cross browser support.

--- src/site/js/print-recipe.js
+++ src/site/js/print-recipe.js
@@ -2,41 +2,47 @@
 
     'use strict';
 
-    function printRecipe(event) {
-        var recipe = $(event.currentTarget).parents('.recipe').clone().get(0),
-            recipeTitle = recipe.querySelector('.title').textContent,
-            content,
-            iframe;
-
-        recipe.querySelector('.recipe-controls').remove();
-
-        iframe = document.body.querySelector('iframe#printable-iframe');
-
-        if (!iframe) {
-            iframe = document.createElement('iframe');
-
-            iframe.setAttribute('id', 'printable-iframe');
-            iframe.classList.add('printable-iframe');
-
-            document.body.appendChild(iframe);
-        }
-
-        content =
-            '<!DOCTYPE html>' +
-                '<head>' +
-                    '<title>' + recipeTitle + '</title>' +
-                '</head>' +
-                '<body>' +
-                    recipe.outerHTML +
-                '</body>' +
-            '</html>';
-
-        iframe.contentWindow.document.open('text/htmlreplace');
-        iframe.contentWindow.document.write(content);
-        iframe.contentWindow.document.close();
+    function iframeLoaded(event) {
+        var iframe = event.currentTarget;
 
         iframe.focus();
         iframe.contentWindow.print();
+    }
+
+    function printRecipe(event) {
+        var $recipe = $(event.currentTarget).parents('.recipe').clone(),
+            $iframe,
+            printableIframe = 'printable-iframe',
+            content,
+            iframe;
+
+        $recipe.find('.recipe-controls').remove();
+
+        $iframe = $('iframe#printable-iframe');
+
+        if (!$iframe.length) {
+            $iframe = $('<iframe id="' + printableIframe + '"></iframe>');
+
+            $('body').append($iframe);
+        }
+
+        iframe = $iframe.get(0);
+
+        iframe.onload = iframeLoaded;
+
+        content =
+            '<html>' +
+                '<head>' +
+                    '<title>' + $recipe.find('.title').text() + '</title>' +
+                '</head>' +
+                '<body>' +
+                    $recipe.get(0).outerHTML +
+                '</body>' +
+            '</html>';
+
+        iframe.contentWindow.document.open();
+        iframe.contentWindow.document.write(content);
+        iframe.contentWindow.document.close();
     }
 
     function init() {
