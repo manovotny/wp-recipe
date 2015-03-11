a0b2798c1143007be5091cf7897d61f052cea543
Michael Novotny <manovotny@gmail.com>
2015-03-11 15:20:35 -0500

    Adds taxonomies array.

--- src/classes/class-wp-recipe-taxonomies.php
+++ src/classes/class-wp-recipe-taxonomies.php
@@ -32,6 +32,27 @@ class WP_Recipe_Taxonomies {
 
     }
 
+    /* Taxonomies
+    ---------------------------------------------- */
+
+    /**
+     * Getter method for recipe taxonomies.
+     *
+     * @return array Recipe taxonomies.
+     */
+    public function get_taxonomies() {
+
+        return array(
+            WP_Recipe_Cooking_Methods_Taxonomy::get_instance()->get_slug(),
+            WP_Recipe_Courses_Taxonomy::get_instance()->get_slug(),
+            WP_Recipe_Cuisines_Taxonomy::get_instance()->get_slug(),
+            WP_Recipe_Diets_Taxonomy::get_instance()->get_slug(),
+            WP_Recipe_Ingredients_Taxonomy::get_instance()->get_slug(),
+            WP_Recipe_Occasions_Taxonomy::get_instance()->get_slug()
+        );
+
+    }
+
     /* Constructor
     ---------------------------------------------------------------------------------- */
 
@@ -70,7 +91,7 @@ class WP_Recipe_Taxonomies {
      */
     public function register_taxonomies() {
 
-        $taxonomies = array(
+        $taxonomies_options = array(
             WP_Recipe_Cooking_Methods_Taxonomy::get_instance()->get_taxonomy_options(),
             WP_Recipe_Courses_Taxonomy::get_instance()->get_taxonomy_options(),
             WP_Recipe_Cuisines_Taxonomy::get_instance()->get_taxonomy_options(),
@@ -79,7 +100,7 @@ class WP_Recipe_Taxonomies {
             WP_Recipe_Occasions_Taxonomy::get_instance()->get_taxonomy_options()
         );
 
-        WP_Taxonomy_Util::get_instance()->create_taxonomies( $taxonomies );
+        WP_Taxonomy_Util::get_instance()->create_taxonomies( $taxonomies_options );
 
     }
 
