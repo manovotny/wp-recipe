a3a7386085a25784609fa5353e0f9770ec3310c2
Michael Novotny <manovotny@gmail.com>
2015-04-03 22:00:15 -0500

    Moves directions to a class.

--- src/classes/class-wp-recipe-directions.php
+++ src/classes/class-wp-recipe-directions.php
@@ -5,20 +5,6 @@ class WP_Recipe_Directions {
     /* Properties
     ---------------------------------------------------------------------------------- */
 
-    /* Id
-    ---------------------------------------------- */
-
-    /**
-     * Getter method for id.
-     *
-     * @return string Recipe directions id.
-     */
-    public function get_id() {
-
-        return str_replace( '-', '_', $this->slug );
-
-    }
-
     /* Instance
     ---------------------------------------------- */
 
@@ -46,34 +32,6 @@ class WP_Recipe_Directions {
 
     }
 
-    /* Nonce
-    ---------------------------------------------- */
-
-    /**
-     * Getter method for nonce.
-     *
-     * @return string Recipe directions nonce.
-     */
-    public function get_nonce() {
-
-        return $this->slug . '-nonce';
-
-    }
-
-    /* Meta Slug
-    ---------------------------------------------- */
-
-    /**
-     * Getter method for meta slug.
-     *
-     * @return string Recipe directions meta slug.
-     */
-    public function get_meta_slug() {
-
-        return '_' . $this->slug;
-
-    }
-
     /* Slug
     ---------------------------------------------- */
 
@@ -84,14 +42,117 @@ class WP_Recipe_Directions {
      */
     protected $slug = 'wp-recipe-directions';
 
-    /**
-     * Getter method for slug.
-     *
-     * @return string Recipe directions slug.
-     */
-    public function get_slug() {
+    /* Constructor
+    ---------------------------------------------------------------------------------- */
 
-        return $this->slug;
+    /**
+     * Initialize class.
+     */
+    public function __construct() {
+
+        add_action( 'add_meta_boxes_recipe', array( $this, 'add_meta_box' ) );
+        add_action( 'save_post_' . WP_Recipe::get_instance()->get_post_type(), array( $this, 'save' ) );
+
+    }
+
+    /* Methods
+    ---------------------------------------------------------------------------------- */
+
+    /**
+     * Adds post cross reference meta box to recipes.
+     */
+    public function add_meta_box() {
+
+        $wp_recipe = WP_Recipe::get_instance();
+
+        add_meta_box(
+            $this->slug,
+            'Directions',
+            array( $this, 'render' ),
+            $wp_recipe->get_post_type(),
+            'normal',
+            'high'
+        );
+
+    }
+
+    /**
+     * Renders meta box.
+     */
+    public function render() {
+
+        global $post;
+
+        wp_nonce_field( $this->slug, $this->get_nonce() );
+
+        $directions = get_post_meta( $post->ID, $this->get_post_meta_key(), true );
+
+        $settings = array(
+            'drag_drop_upload' => true,
+            'textarea_rows' => 8
+        );
+
+        wp_editor( $directions, $this->get_id(), $settings );
+
+    }
+
+    /**
+     * Saves meta box.
+     *
+     * @param string $post_id Post id.
+     */
+    public function save( $post_id ) {
+
+        $wp_post_type_util = WP_Post_Type_Util::get_instance();
+        $wp_recipe = WP_Recipe::get_instance();
+
+        if ( ! $wp_post_type_util->is_post_type_saving_post_meta( $wp_recipe->get_post_type() ) ) {
+
+            return;
+
+        }
+
+        if ( $wp_post_type_util->can_save_post_meta( $post_id, $this->slug, $this->get_nonce() ) ) {
+
+            update_post_meta( $post_id, $this->get_post_meta_key(), $_POST[ $this->get_id() ] );
+
+        }
+
+    }
+
+    /* Helpers
+    ---------------------------------------------------------------------------------- */
+
+    /**
+     * Gets editor id.
+     *
+     * @return string Recipe directions id.
+     */
+    private function get_id() {
+
+        return str_replace( '-', '_', $this->slug );
+
+    }
+
+    /**
+     * Gets nonce.
+     *
+     * @return string Recipe directions nonce.
+     */
+    private function get_nonce() {
+
+        return $this->slug . '-nonce';
+
+    }
+
+    /**
+     * Gets post meta key.
+     *
+     * @return string Recipe directions post meta key.
+     */
+    private function get_post_meta_key() {
+
+        return '_' . $this->slug;
 
     }
 
